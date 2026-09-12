30ee547c828ce3c259ee9851e8e56a58fcbc9579
Bright Poku <bpoku@nvidia.com>
2026-09-11 23:07:18 -0400

    docs(observability): document assertion helpers
    Signed-off-by: Bright Poku <bpoku@nvidia.com>

--- src/libraries/go/lib/pkg/icms-translate/translate/task/translate_all_test.go
+++ src/libraries/go/lib/pkg/icms-translate/translate/task/translate_all_test.go
@@ -333,7 +333,6 @@ func findTaskPodByName(t *testing.T, objs []metav1.Object, name string) *corev1.
 	return nil
 }
 
-// findTaskContainerByName returns the named task container or fails the test.
 func findTaskContainerByName(t *testing.T, containers []corev1.Container, name string) corev1.Container {
 	t.Helper()
 
