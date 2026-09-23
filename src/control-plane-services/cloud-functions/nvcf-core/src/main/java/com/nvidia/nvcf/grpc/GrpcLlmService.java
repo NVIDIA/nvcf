36cedc4f3599d12b27883ac8d2533c6b17ec1665
Priya Selvaganesan <1836417+priyaselvaganesan@users.noreply.github.com>
2026-09-23 21:33:57 +0000

    feat(control-plane): adopt Spring Boot native gRPC support (#2068)
    Signed-off-by: priyaselvaganesan <pselvaganesa@nvidia.com>

--- src/control-plane-services/cloud-functions/nvcf-core/src/main/java/com/nvidia/nvcf/grpc/GrpcLlmService.java
+++ src/control-plane-services/cloud-functions/nvcf-core/src/main/java/com/nvidia/nvcf/grpc/GrpcLlmService.java
@@ -40,7 +40,7 @@ import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.devh.boot.grpc.server.service.GrpcService;
+import org.springframework.grpc.server.service.GrpcService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthenticationToken;
